49a545cf3d1a88cfab70fc21ccb2a2b201db8f42
Felix Schelling <felix.schelling@protonmail.com>
2024-10-20 12:55:55 +0200

    added price calc tests

---
+++ tests/test_price_calculation.py
@@ -0,0 +1,187 @@
+from gdocs_4_ski_automation.core.price_calculation import get_price
+from gdocs_4_ski_automation.core.ctypes import Course, Participant, Name
+
+
+
+
+def test_price_calc0():
+
+    participants = [Participant(
+        name = Name("Max", "Mustermann"),
+        age=10,
+        course=Course.SKI,
+        pre_course="",
+        notes="",
+    )]
+    date = "01.01.2025 00:00:00"
+
+    prices = {
+        "Zwergerl": 100,
+        "Kind": 200,
+        "Erwachsen": 300,
+        "FruehbucherRabatt": 20,
+        "FruehbucherRabattDatum": "01.01.2024",
+    }
+    assert get_price(participants,date,prices=prices) == 200
+
+
+def test_price_calc1():
+
+    participants = [Participant(
+        name = Name("Max", "Mustermann"),
+        age=10,
+        course=Course.SKI,
+        pre_course="",
+        notes="",
+    ),Participant(
+        name = Name("Maxi", "Mustermanni"),
+        age=9,
+        course=Course.SKI,
+        pre_course="",
+        notes="",
+    )]
+    date = "20.12.2024 00:00:00"
+
+    prices = {
+        "Zwergerl": 100,
+        "Kind": 200,
+        "Erwachsen": 300,
+        "FruehbucherRabatt": 20,
+        "FruehbucherRabattDatum": "01.01.2025",
+    }
+    assert get_price(participants,date,prices=prices) == 360
+
+def test_price_calc2():
+
+    participants = [Participant(
+        name = Name("Max", "Mustermann"),
+        age=20,
+        course=Course.SKI,
+        pre_course="",
+        notes="",
+    ),Participant(
+        name = Name("Maxi", "Mustermanni"),
+        age=19,
+        course=Course.SKI,
+        pre_course="",
+        notes="",
+    )]
+    date = "20.12.2024 00:00:00"
+
+    prices = {
+        "Zwergerl": 100,
+        "Kind": 200,
+        "Erwachsen": 300,
+        "FruehbucherRabatt": 20,
+        "FruehbucherRabattDatum": "01.01.2025",
+    }
+    assert get_price(participants,date,prices=prices) == 560
+
+def test_price_calc3():
+
+    participants = [Participant(
+        name = Name("Max", "Mustermann"),
+        age=2,
+        course=Course.SKI,
+        pre_course="",
+        notes="",
+    ),Participant(
+        name = Name("Maxi", "Mustermanni"),
+        age=19,
+        course=Course.SKI,
+        pre_course="",
+        notes="",
+    )]
+    date = "20.12.2024 00:00:00"
+
+    prices = {
+        "Zwergerl": 100,
+        "Kind": 200,
+        "Erwachsen": 300,
+        "FruehbucherRabatt": 20,
+        "FruehbucherRabattDatum": "01.01.2025",
+    }
+    assert get_price(participants,date,prices=prices) == 460
+
+
+def test_price_calc4():
+
+    participants = [Participant(
+        name = Name("Max", "Mustermann"),
+        age=2,
+        course=Course.ZWEGERL_SNOWBOARD,
+        pre_course="",
+        notes="",
+    ),Participant(
+        name = Name("Maxi", "Mustermanni"),
+        age=19,
+        course=Course.SKI,
+        pre_course="",
+        notes="",
+    ),
+    Participant(
+        name = Name("Maxi", "Mustermanni"),
+        age=9,
+        course=Course.SNOWBOARD,
+        pre_course="",
+        notes="",
+    ),
+     Participant(
+        name = Name("Maxi", "Mustermanni"),
+        age=5,
+        course=Course.ZWEGERL,
+        pre_course="",
+        notes="",
+    )]
+    date = "02.01.2025 00:00:00"
+
+    prices = {
+        "Zwergerl": 100,
+        "Kind": 200,
+        "Erwachsen": 300,
+        "FamilienRabatt": 5,
+        "FruehbucherRabatt": 20,
+        "FruehbucherRabattDatum": "01.01.2025",
+    }
+    assert get_price(participants,date,prices=prices) == (100+100+300+200)-(2*5)
+
+def test_price_calc5():
+
+    participants = [Participant(
+        name = Name("Max", "Mustermann"),
+        age=2,
+        course=Course.ZWEGERL_SNOWBOARD,
+        pre_course="",
+        notes="",
+    ),Participant(
+        name = Name("Maxi", "Mustermanni"),
+        age=19,
+        course=Course.SKI,
+        pre_course="",
+        notes="",
+    ),
+    Participant(
+        name = Name("Maxi", "Mustermanni"),
+        age=9,
+        course=Course.SNOWBOARD,
+        pre_course="",
+        notes="",
+    ),
+     Participant(
+        name = Name("Maxi", "Mustermanni"),
+        age=5,
+        course=Course.ZWEGERL,
+        pre_course="",
+        notes="",
+    )]
+    date = "20.12.2024 00:00:00"
+
+    prices = {
+        "Zwergerl": 100,
+        "Kind": 200,
+        "Erwachsen": 300,
+        "FamilienRabatt": 5,
+        "FruehbucherRabatt": 20,
+        "FruehbucherRabattDatum": "01.01.2025",
+    }
+    assert get_price(participants,date,prices=prices) == (100+100+300+200)-(4*20)-(2*5)
